30457d8538584486c6812c65c5645ba758354458
qa-mpeters <mike.peters@qualityaspirators.com>
2024-04-18 10:33:29 -0500

    Initial code commit

---
+++ NumericTextBox/NumericTextBox.cs
@@ -0,0 +1,15 @@
+﻿namespace NumericTextBox
+{
+    public partial class NumericTextBox : TextBox
+    {
+        public NumericTextBox()
+        {
+            BackColor = Color.Pink;
+            ForeColor = Color.Black;
+
+        }
+
+        public bool HasDecimal { get; set; }
+
+    }
+}
